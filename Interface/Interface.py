722859cdde0ac470050d2e63855b370ff6d0eb4e
Carlos Eduardo <89090426+Prexede@users.noreply.github.com>
2024-03-25 19:02:11 -0300

    Att Interface
    Adicionado botão para aparecer gráfico com informações dos angulos

--- Interface/Interface.py
+++ Interface/Interface.py
@@ -1,9 +1,26 @@
 import tkinter as tk
 from tkinter import PhotoImage
-import awesometkinter as atk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-def exibir_mensagem():
-    label['text'] = 'Olá, Tkinter!'
+
+def Graficos():
+    
+    # Cria um gráfico usando Matplotlib
+    fig = Figure(figsize=(5, 4), dpi=100)
+    plot = fig.add_subplot(1, 1, 1)
+    plot.plot([0, 1, 2, 3], [0, 1, 4, 9])  # Exemplo de dados para o gráfico
+
+    # Incorpora o gráfico na janela do Tkinter
+    canvas = FigureCanvasTkAgg(fig, master=janela)  # A 'master' é a janela do Tkinter
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    
+    
+def Conexao():
+    grafico = Graficos.canvas.get_tk_widget()
+    grafico.destroy()
+
 
 # Cria a janela principal
 janela = tk.Tk()
@@ -14,9 +31,6 @@ largura = 1280
 altura = 720
 # Configura a resolução da janela
 janela.geometry(f'{largura}x{altura}')
-
-
-
 
 # Carrega a imagem de fundo
 caminho_imagem = r'C:\Users\carlo\OneDrive\Documentos\Drive\Faculdade\IC\Interface\Background\FundoTelemetria.png'  # Substitua com o caminho correto da sua imagem
@@ -30,36 +44,28 @@ imagem_fundo = imagem_fundo.subsample(1)  # Ajuste o fator de subsample conforme
 label_fundo = tk.Label(janela, image=imagem_fundo)
 label_fundo.place(x=0, y=0, relwidth=1, relheight=1)
 
-
-
 #########################################################################################################3
 # Cria um Frame3d para a barra lateral
-barra_lateral = atk.Frame3d(janela, width=100, height=300, bg='#5179AA', corner_radius=10)
+barra_lateral = tk.Frame(janela, width=100, height=300, bg='#5179AA')
 barra_lateral.pack(side='left', fill='none')  # Posiciona a barra lateral à esquerda
 
 # Adiciona botões à barra lateral
+
+################# CONEXAO ####################
 # Carrega a imagem para o botão
-caminho_imagemBotaoWifi = r'C:\Users\carlo\OneDrive\Documentos\Drive\Faculdade\IC\Interface\Background\Conexao.png'  # Substitua pelo caminho correto da sua imagem
+caminho_imagemBotaoWifi = r'C:\Users\carlo\OneDrive\Documentos\Drive\Faculdade\IC\Interface\Background\Conexao.png'
 imagem_botaoWifi = PhotoImage(file=caminho_imagemBotaoWifi)
-
 # Cria um botão com a imagem e o posiciona no Frame3d
-botao1 = atk.Button3d(barra_lateral, image=imagem_botaoWifi, compound='center')
-botao1.pack(pady=100)  # Adiciona espaço vertical entre os botões
+botao1 = tk.Button(barra_lateral, image=imagem_botaoWifi, compound='center', command=Conexao)
+botao1.pack(pady=50)  # Adiciona espaço vertical entre os botões
 
-# Cria um botão de texto e o posiciona no Frame3d
-botao2 = atk.Button3d(barra_lateral, text='Botão 2')
-botao2.pack(pady=100)
+############### GRAFICOS #####################################################
+caminho_imagemBotaoGrafico = r'C:\Users\carlo\OneDrive\Documentos\Drive\Faculdade\IC\Interface\Background\Grafico.png' 
+imagem_botaoGrafico = PhotoImage(file=caminho_imagemBotaoGrafico)
+botao2 = tk.Button(barra_lateral, image=imagem_botaoGrafico, compound='center', command=Graficos)
+botao2.pack(pady=50)
+
 ############################################################################################################
-
-
-# Cria um rótulo (label) na janela
-label = tk.Label(janela, text='Clique no botão')
-label.pack()
-
-# Cria um botão que chama a função exibir_mensagem
-botao = tk.Button(janela, text='Clique aqui', command=exibir_mensagem)
-botao.pack()
 
 # Inicia o loop principal da interface gráfica
 janela.mainloop()
-
