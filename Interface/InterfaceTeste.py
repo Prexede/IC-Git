48d34731aacb19534bb08c629aac95f143c4a2f3
Carlos Eduardo <89090426+Prexede@users.noreply.github.com>
2024-08-24 00:33:07 -0300

    att interface
    Adicao de leitura serial para a interface/Leitura dos dados do esp32

--- Interface/InterfaceTeste.py
+++ Interface/InterfaceTeste.py
@@ -1,13 +1,13 @@
 import tkinter as tk
-from tkinter import ttk,PhotoImage
+from tkinter import ttk, PhotoImage
 from tkinter import filedialog
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.animation import FuncAnimation
 import random
 import matplotlib.pyplot as plt
+import serial
 import serial.tools.list_ports
-
 
 def resetar_dados():
     global x_data, y_data, x_data2, y_data2, x_data3, y_data3
@@ -21,7 +21,6 @@ def resetar_dados():
     ax2.clear()
     ax3.clear()
     canvas.draw()
-    
 
 def salvar_dados(nome_arquivo, x_data, y_data, x_data2, y_data2, x_data3, y_data3):
     with open(nome_arquivo, 'w') as f:
@@ -34,15 +33,14 @@ def salvar_dados(nome_arquivo, x_data, y_data, x_data2, y_data2, x_data3, y_data
         f.write("\nGráfico 3:\n")
         for x, y in zip(x_data3, y_data3):
             f.write(f"{x}\t{y}\n")
-            
-            
+
 def atualizar_grafico(i):
     global serial_conn
     if serial_conn and serial_conn.is_open:
         try:
             dados = serial_conn.readline().decode('utf-8').strip()
             if dados:
-                valores = [float(val) for val in dados.split()]
+                valores = [int(val) for val in dados.split(',')]
                 if len(valores) == 3:
                     x_data.append(i)
                     y_data.append(valores[0])
@@ -52,6 +50,7 @@ def atualizar_grafico(i):
                     y_data3.append(valores[2])
         except Exception as e:
             print(f"Erro ao ler dados: {e}")
+    
     # GRAFICO 1
     ax.clear()
     ax.plot(x_data, y_data)
@@ -66,7 +65,7 @@ def atualizar_grafico(i):
     ax2.set_xlabel("Tempo[ms]")
     ax2.set_ylabel("Angulo[graus]")
     ax2.grid(axis='both')
-    ax2.set_title("Raw")
+    ax2.set_title("Esqueci")
     
     # GRAFICO 3
     ax3.clear()
@@ -74,16 +73,15 @@ def atualizar_grafico(i):
     ax3.set_xlabel("Tempo[ms]")
     ax3.set_ylabel("Angulo[graus]")
     ax3.grid(True)
-    ax3.set_title("Yaw")
-    
-    
+    ax3.set_title("YAW")
+
 def Graficos():
-    global canvas, ax, x_data, y_data, ax2, x_data2, y_data2, ax3, x_data3, y_data3, widgets_criados,i
+    global canvas, ax, x_data, y_data, ax2, x_data2, y_data2, ax3, x_data3, y_data3, widgets_criados, i
     x_data, y_data = [], []
     x_data2, y_data2 = [], []
     x_data3, y_data3 = [], []
     
-    #Verifica se o botão está apertado 
+    # Verifica se o botão está apertado 
     if canvas:
         return
     
@@ -104,14 +102,13 @@ def Graficos():
     widgets_criados.append(canvas_widget)
     
     # Configura a animação
-    ani1 = FuncAnimation(fig, atualizar_grafico, interval=50,cache_frame_data=False)
+    ani1 = FuncAnimation(fig, atualizar_grafico, interval=1000, frames=30)
     
     # Adiciona os botões de salvar
     global botao_salvar1
     botao_salvar1 = tk.Button(barra_lateral, text="Salvar Dados Gráfico 1", command=lambda: salvar_dados('grafico1.txt', x_data, y_data, x_data2, y_data2, x_data3, y_data3))
     botao_salvar1.pack(side=tk.LEFT, padx=10, pady=10)
     widgets_criados.append(botao_salvar1)
-    
     
     # Adiciona os botões de Resetar grafico
     global botao_Reset
@@ -124,16 +121,16 @@ def Graficos():
 def listar_portas():
     portas = serial.tools.list_ports.comports()
     return [porta.device for porta in portas]
-   
+
 def Conexao():
-    global widgets_criados, canvas
+    global widgets_criados, canvas, texto_status, serial_conn, label_dados
     # Apaga todos os widgets criados pelo botão "Gráficos"
     for widget in widgets_criados:
         widget.destroy()
         canvas = None
     widgets_criados = []
     
-# Cria um frame centralizado
+    # Cria um frame centralizado
     frame_central = tk.Frame(janela)
     frame_central.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
     widgets_criados.append(frame_central)
@@ -153,37 +150,25 @@ def Conexao():
     
     label_dados = tk.Label(frame_central, text="Dados do Serial:")
     label_dados.pack(pady=10)
-    def conectar_porta(porta_selecionada):
-        global serial_conn
-        if porta_selecionada:
-            try:
-                serial_conn = serial.Serial(porta_selecionada, 9600, timeout=0.1)
-                texto_status.config(text=f"Conectado à {porta_selecionada}")
-                janela.after(100, ler_dados_serial)  # Chama a função para ler dados do serial
-            except Exception as e:
-                texto_status.config(text=f"Erro: {e}")
-        else:
-            texto_status.config(text="Nenhuma porta selecionada")
-    def ler_dados_serial():
-        global serial_conn
-        if serial_conn and serial_conn.is_open:
-            try:
-                dados = serial_conn.readline().decode('utf-8').strip()
-                if dados:
-                    label_dados.config(text=f"Dados do Serial: {dados}")
-            except Exception as e:
-                label_dados.config(text=f"Erro ao ler dados: {e}")
-        janela.after(100, ler_dados_serial)  # Chama a função novamente após 1 segundo
-        
-        
+
+def conectar_porta(porta_selecionada):
+    global serial_conn
+    if porta_selecionada:
+        try:
+            serial_conn = serial.Serial(porta_selecionada, 9600, timeout=1)
+            texto_status.config(text=f"Conectado à {porta_selecionada}")
+        except Exception as e:
+            texto_status.config(text=f"Erro: {e}")
+    else:
+        texto_status.config(text="Nenhuma porta selecionada")
 
 # Cria a janela principal
 janela = tk.Tk()
 janela.title('Exemplo Tkinter')
 
 # Define a largura e a altura desejadas
-largura = 1920
-altura = 1080
+largura = 1280
+altura = 720
 # Configura a resolução da janela
 janela.geometry(f'{largura}x{altura}')
 
@@ -192,38 +177,26 @@ caminho_imagem = r'C:\Users\carlo\OneDrive\Documentos\Drive\Faculdade\IC\Interfa
 imagem_fundo = PhotoImage(file=caminho_imagem)
 
 # Redimensiona a imagem para cobrir toda a janela
-imagem_fundo = imagem_fundo.zoom(2)  # Ajuste o fator de zoom conforme necessário
+imagem_fundo = imagem_fundo.zoom(1)  # Ajuste o fator de zoom conforme necessário
+imagem_fundo = imagem_fundo.subsample(1)  # Ajuste o fator de subsample conforme necessário
+
 # Configura a imagem de fundo
 label_fundo = tk.Label(janela, image=imagem_fundo)
-label_fundo.place(x=0, y=0, relwidth=1, relheight=1)
+label_fundo.place(relwidth=1, relheight=1)
 
+# Barra lateral para os botões
+barra_lateral = tk.Frame(janela)
+barra_lateral.pack(side=tk.LEFT, fill=tk.Y)
 
-#########################################################################################################3
-# Cria um Frame3d para a barra lateral
-barra_lateral = tk.Frame(janela, width=100, height=300, bg='#5179AA')
-barra_lateral.pack(side='left', fill='none')  # Posiciona a barra lateral à esquerda
+# Botão para mostrar gráficos
+botao_graficos = tk.Button(barra_lateral, text="Mostrar Gráficos", command=Graficos)
+botao_graficos.pack(side=tk.TOP, padx=10, pady=10)
 
-# Adiciona botões à barra lateral
+# Botão para conexão
+botao_conexao = tk.Button(barra_lateral, text="Conexão", command=Conexao)
+botao_conexao.pack(side=tk.TOP, padx=10, pady=10)
 
-################# CONEXAO ####################
-# Carrega a imagem para o botão
-caminho_imagemBotaoWifi = r'C:\Users\carlo\OneDrive\Documentos\Drive\Faculdade\IC\Interface\Background\Conexao.png'
-imagem_botaoWifi = PhotoImage(file=caminho_imagemBotaoWifi)
-# Cria um botão com a imagem e o posiciona no Frame3d
-botao1 = tk.Button(barra_lateral, image=imagem_botaoWifi, compound='center', command=Conexao)
-botao1.pack(pady=50)  # Adiciona espaço vertical entre os botões
-
-############### GRAFICOS #####################################################
-caminho_imagemBotaoGrafico = r'C:\Users\carlo\OneDrive\Documentos\Drive\Faculdade\IC\Interface\Background\Grafico.png' 
-imagem_botaoGrafico = PhotoImage(file=caminho_imagemBotaoGrafico)
-botao2 = tk.Button(barra_lateral, image=imagem_botaoGrafico, compound='center', command=Graficos)
-botao2.pack(pady=50)
-
-############################################################################################################
-
-# Lista para armazenar widgets criados pelo botão "Gráficos"
+# Lista de widgets criados
 widgets_criados = []
-canvas = None
 
-# Inicia o loop principal da interface gráfica
 janela.mainloop()
